c20bb17a7385e1ecb199e1d51fbb5a09afe7922d
AdamMaley <adamemaley@yahoo.com>
2020-01-06 16:08:46 -0500

    stuck on mapping still

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -15,6 +15,9 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       const numberComponents = numbers.map(number => <Number />)
+
+       
     </div>
   );
 };
